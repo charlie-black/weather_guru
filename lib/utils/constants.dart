b6eaeba937a6ea932272c2a9ea4b81728bd3d6ae
charlie-black <boompiccasso@gmail.com>
2024-05-30 14:15:12 +0300

    app constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+
+final Color kBackgroundColor = HexColor("#1e1f2f");
+final Color kNarrationContainerColor = HexColor("#202940");
+final Color kNarrationTextColor = HexColor("#778096");
+final Color kPrimaryColor = HexColor("#dbdce0");
+final Color kIconColor = HexColor("#4caf50");
+final Color kActiveColor = HexColor('#852e9f');
+final Color kDangerColor = HexColor('#f44335');
+final Color kDividerColor = HexColor('#778096');
+final Color kCardColor = HexColor('#27293c');
+
+
+
+
+
+
+
