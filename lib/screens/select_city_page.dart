b796b5be65ec391431a1a8f137bccdcafec4cf50
charlie-black <boompiccasso@gmail.com>
2024-05-31 13:47:58 +0300

    refactored the ui

--- lib/screens/select_city_page.dart
+++ lib/screens/select_city_page.dart
@@ -1,11 +1,15 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:lottie/lottie.dart';
 import 'package:sizer/sizer.dart';
 import 'package:weather_guru/environment.dart';
+import 'package:weather_guru/screens/offline_data/saved_city_weather_data.dart';
 import 'package:weather_guru/screens/today_weather_page.dart';
 import 'package:weather_guru/utils/constants.dart';
+
+import '../utils/api_response.dart';
+import '../utils/internet_connectivity.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,37 +22,87 @@ class _HomePageState extends State<HomePage> {
   final TextEditingController cityController = TextEditingController();
   List<String> _citySuggestions = [];
 
-  void fetchCitySuggestions(String input) async {
+  Future<ApiResponse> fetchCitySuggestions(String input) async {
     if (input.length < 3) {
       setState(() {
         _citySuggestions.clear();
       });
-      return;
+      return ApiResponse(
+          content: null,
+          errorMessage: "Input must be at least 3 characters long.");
     }
 
     String apiKey = citiesXApiKey;
     String apiUrl = "$citiesEndpointUrl$input";
-    Map<String, String> headers = {
-      'X-Api-Key': apiKey,
-    };
-    http.Response response =
-    await http.get(Uri.parse(apiUrl), headers: headers);
-    if (response.statusCode == 200) {
-      List<dynamic> jsonData = json.decode(response.body);
-      setState(() {
-        _citySuggestions =
-            jsonData.map((e) => e['name']).toList().cast<String>();
-      });
-    } else {
-      print('Request failed with status: ${response.statusCode}');
+
+    try {
+      var connectivityNotifier = ConnectivityNotifier();
+      bool isConnected = await connectivityNotifier.checkInternetConnection();
+      if (!isConnected) {
+        return ApiResponse(
+            content: null, errorMessage: "No internet connection");
+      }
+
+      Response response = await Dio()
+          .get(apiUrl, options: Options(headers: {'X-Api-Key': apiKey}));
+
+      if (response.statusCode == 200) {
+        List<dynamic> jsonData = response.data;
+        setState(() {
+          _citySuggestions = jsonData.map((e) => e['name'].toString()).toList();
+        });
+        return ApiResponse(content: _citySuggestions, errorMessage: "");
+      } else {
+        return ApiResponse(
+            content: null,
+            errorMessage: 'Request failed with status: ${response.statusCode}');
+      }
+    } catch (e) {
+      return ApiResponse(
+          content: null, errorMessage: 'Request failed with error: $e');
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: SpeedDial(
+          icon: Icons.menu,
+          activeIcon: Icons.close,
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+          activeBackgroundColor: Colors.black,
+          activeForegroundColor: Colors.white,
+          visible: true,
+          closeManually: false,
+          curve: Curves.bounceIn,
+          overlayColor: Colors.black,
+          overlayOpacity: 0.5,
+          elevation: 8.0,
+          shape: const CircleBorder(),
+          children: [
+            SpeedDialChild(
+              child: const Icon(Icons.remove_red_eye_outlined),
+              backgroundColor: Colors.black,
+              foregroundColor: Colors.white,
+              label: 'View Offline Data',
+              labelStyle: TextStyle(color: kBackgroundColor, fontSize: 12),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const OfflineWeatherPage(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       backgroundColor: kPrimaryColor,
-
       body: SingleChildScrollView(
         child: Container(
           color: kPrimaryColor,
@@ -58,11 +112,16 @@ class _HomePageState extends State<HomePage> {
               SizedBox(
                 height: 20.h,
               ),
-              Lottie.asset('assets/json/compass.json',
-                  height: 150, width: 150),
-            const Padding(
+              Lottie.asset('assets/json/compass.json', height: 150, width: 150),
+              const Padding(
                 padding: EdgeInsets.all(8.0),
-                child: Text('Weather Guru',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),),
+                child: Text(
+                  'Weather Guru',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -90,28 +149,35 @@ class _HomePageState extends State<HomePage> {
                     }
                   },
                   decoration: InputDecoration(
-
                     suffixIcon: IconButton(
                       onPressed: () {
-                        if(cityController.text.isEmpty){
+                        if (cityController.text.isEmpty) {
                           ScaffoldMessenger.of(context).showSnackBar(
                             const SnackBar(
-                              content: Text("Enter city name before proceeding",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 15),),
+                              content: Text(
+                                "Enter city name before proceeding",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15),
+                              ),
                             ),
                           );
-                        }else {
+                        } else {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) =>
-                                  WeatherPage(
-                                    cityName: cityController.text,
-                                  ),
+                              builder: (context) => WeatherPage(
+                                cityName: cityController.text,
+                              ),
                             ),
                           );
                         }
                       },
-                      icon: const Icon(Icons.search,color: Colors.black,),
+                      icon: const Icon(
+                        Icons.search,
+                        color: Colors.black,
+                      ),
                     ),
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
@@ -119,19 +185,15 @@ class _HomePageState extends State<HomePage> {
                     ),
                     labelText: 'Type in a City',
                     labelStyle: const TextStyle(
-                        fontWeight: FontWeight.normal,
-                        color: Colors.black),
+                        fontWeight: FontWeight.normal, color: Colors.black),
                     enabledBorder: const OutlineInputBorder(
-                      borderSide:
-                      BorderSide(width: 0, color: Colors.black),
+                      borderSide: BorderSide(width: 0, color: Colors.black),
                     ),
                     focusedBorder: const OutlineInputBorder(
-                      borderSide:
-                      BorderSide(width: 0, color: Colors.black),
+                      borderSide: BorderSide(width: 0, color: Colors.black),
                     ),
                   ),
-                  style: const TextStyle(
-                      color: Colors.black, fontSize: 15),
+                  style: const TextStyle(color: Colors.black, fontSize: 15),
                 ),
               ),
               const SizedBox(height: 16.0),
@@ -141,7 +203,10 @@ class _HomePageState extends State<HomePage> {
                   shrinkWrap: true,
                   itemBuilder: (context, index) {
                     return ListTile(
-                      title: Text(_citySuggestions[index],style: const TextStyle(color: Colors.black),),
+                      title: Text(
+                        _citySuggestions[index],
+                        style: const TextStyle(color: Colors.black),
+                      ),
                       onTap: () {
                         cityController.text = _citySuggestions[index];
                         setState(() {
